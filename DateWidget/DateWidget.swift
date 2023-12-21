788ec1a405dcfc2e6957719845567ba62831ee3a
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2023-12-21 13:48:54 -0500

    SimpleDateWidget: update 2.0
    - Support AppIntents.
    - Support AppIntentTimelineProvider.
    - Support WidgetConfigurationIntent.
    - Support AppIntentConfiguration.
    - Add Button to tell DateAppIntent to trigger copy current to clipboard in the homework format Exp 00/00/000.
    - Fix all spelling mistake.
    - Add build instruction for over version of for older version of macOS that don’t support interactive widget.

--- DateWidget/DateWidget.swift
+++ DateWidget/DateWidget.swift
@@ -5,21 +5,34 @@
 //  Created by BAproductions on 1/18/22.
 //
 
-import WidgetKit
 import SwiftUI
 import Intents
+import WidgetKit
+import AppIntents
 
-struct Provider: TimelineProvider {
+struct DateAppIntent: AppIntent, WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Copy the current date in the 00/00/00 format"
+    static var description = IntentDescription("Copy the current date in the 00/00/00 format ")
+    static let currentDate = Date()
+    
+    func perform() async throws -> some IntentResult {
+        let pasteBoard = NSPasteboard.general
+        pasteBoard.clearContents()
+        pasteBoard.setString("\(DateAppIntent.currentDate.formatDT(format: "MM/dd/yyyy"))", forType: NSPasteboard.PasteboardType.string)
+        return .result()
+    }
+}
+
+struct Provider: AppIntentTimelineProvider {
     func placeholder(in context: Context) -> CurrentDate {
         CurrentDate(date: Date())
     }
 
-    func getSnapshot(in context: Context, completion: @escaping (CurrentDate) -> ()) {
-        let entry = CurrentDate(date: Date())
-        completion(entry)
+    func snapshot(for configuration: DateAppIntent, in context: Context) async -> CurrentDate {
+        CurrentDate(date: Date())
     }
 
-    func getTimeline(in context: Context, completion: @escaping (Timeline<CurrentDate>) -> ()) {
+    func timeline(for configuration: DateAppIntent, in context: Context) async -> Timeline<CurrentDate> {
         var entries: [CurrentDate] = []
 
         // Get the current date
@@ -45,11 +58,9 @@ struct Provider: TimelineProvider {
         // Calculate the refresh date (12 AM of the next day)
         let nextRefreshDate = Calendar.current.date(byAdding: .day, value: 1, to: next12AM)!
 
-        let timeline = Timeline(entries: entries, policy: .after(nextRefreshDate))
-        completion(timeline)
+        return Timeline(entries: entries, policy: .after(nextRefreshDate))
     }
 }
-
 
 struct CurrentDate: TimelineEntry {
     let date: Date
@@ -57,6 +68,7 @@ struct CurrentDate: TimelineEntry {
 
 struct DateWidgetEntryView : View {
     @Environment(\.widgetFamily) var widgetFamily
+
     var entry: Provider.Entry
 
     var body: some View {
@@ -65,17 +77,24 @@ struct DateWidgetEntryView : View {
             widgetWiew()
          default:
             widgetWiew()
+
         }
     }
 
     @ViewBuilder func widgetWiew() -> some View {
-        VStack(alignment: .center, spacing: 0){
-            Text(entry.date, format: Date.FormatStyle().weekday().month().day().year())
-                .font(.largeTitle)
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+        Button(intent: DateAppIntent()) {
+            VStack(alignment: .center, spacing: 0){
+                Text(entry.date, format: Date.FormatStyle().weekday().month().day().year())
+                    .font(.largeTitle)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+            }
+            .padding(0)
+            .contentMargins(0)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
         }
-        .contentShape(Rectangle())
-        .padding(0)
+        .padding(-50)
+        .contentMargins(-50)
+        .buttonStyle(.plain)
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
         .foregroundColor(.accentColor)
         .containerBackground(for: .widget) {
@@ -90,13 +109,17 @@ struct DateWidget: Widget {
     let kind: String = "DateWidget"
 
     var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+        AppIntentConfiguration(
+            kind: kind,
+            intent: DateAppIntent.self,
+            provider: Provider()
+        ) { entry in
             DateWidgetEntryView(entry: entry)
         }
-        .containerBackgroundRemovable(false)
+        .containerBackgroundRemovable(true)
+        .supportedFamilies([.systemMedium])
         .configurationDisplayName("Date Widget")
         .description("Display The Ccrrent Date.")
-        .supportedFamilies([.systemMedium])
     }
 }
 
